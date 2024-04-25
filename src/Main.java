303b564901f3a06f376e8bbd00eec00eb26b3708
Aidos Zhasulan <aidoszhasulan155@gmail.com>
2024-04-25 19:14:15 +0300

    checkArrayLisst

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        MyLinkedList<Integer> list = new MyLinkedList<>();
+        MyArrayList<Integer> list = new MyArrayList<>();
 
         // Add elements to the list
         list.addElement(1);
@@ -12,35 +12,34 @@ public class Main {
         // Print the elements in the list
         System.out.println("Elements in the list:");
         for (int i = 0; i < list.getSize(); i++) {
-            System.out.println("Index " + i + ": " + list.getData(i));
+            System.out.println("Index " + i + ": " + list.getElement(i));
         }
 
-        // Test addElementAtIndex
-        list.addElementAtIndex(2, 10);
+        // Add an element at a specific index
+        list.addElement(10, 2);
+
+        // Print the elements in the list after adding an element at index 2
         System.out.println("\nElements in the list after adding 10 at index 2:");
         for (int i = 0; i < list.getSize(); i++) {
-            System.out.println("Index " + i + ": " + list.getData(i));
+            System.out.println("Index " + i + ": " + list.getElement(i));
         }
 
-        // Test remove
-        list.remove(3);
+        // Remove an element at a specific index
+        list.removeElement(3);
+
+        // Print the elements in the list after removing an element at index 3
         System.out.println("\nElements in the list after removing element at index 3:");
         for (int i = 0; i < list.getSize(); i++) {
-            System.out.println("Index " + i + ": " + list.getData(i));
+            System.out.println("Index " + i + ": " + list.getElement(i));
         }
 
-        // Test contains
-        System.out.println("\nDoes the list contain 5? " + list.contains(5));
-
-        // Test indexOf
-        System.out.println("Index of element 3: " + list.indexOf(3));
-
-
-        // Test clear
+        // Clear the list
         list.clear();
-        System.out.println("\nList after clearing:");
+
+        // Print the elements in the list after clearing
+        System.out.println("\nElements in the list after clearing:");
         for (int i = 0; i < list.getSize(); i++) {
-            System.out.println("Index " + i + ": " + list.getData(i));
+            System.out.println("Index " + i + ": " + list.getElement(i));
         }
     }
 
