dfd74fc0f31a5d3e880ebfe844004f6fead110da
Aidos Zhasulan <aidoszhasulan155@gmail.com>
2024-04-26 23:24:56 +0300

    Test queque

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        // Create a stack of integers
-        MyStack<Integer> stack = new MyStack<>();
+        // Create a queue of integers
+        MyQueque<Integer> queue = new MyQueque<>();
 
-        // Push elements onto the stack
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
+        // Enqueue elements into the queue
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
 
-        // Print the size of the stack
-        System.out.println("Size of the stack: " + stack.getSize());
+        // Print the size of the queue
+        System.out.println("Size of the queue: " + queue.getSize());
 
-        // Print the top element of the stack (peek)
-        System.out.println("Top element of the stack: " + stack.peek());
+        // Print the front element of the queue (peek)
+        System.out.println("Front element of the queue: " + queue.peek());
 
-        // Pop elements from the stack
-        System.out.println("Pop elements from the stack:");
-        while (!stack.empty()) {
-            System.out.println(stack.pop());
+        // Dequeue elements from the queue
+        System.out.println("Dequeue elements from the queue:");
+        while (!queue.isEmpty()) {
+            System.out.println(queue.dequeue());
         }
 
-        // Check if the stack is empty
-        System.out.println("Is the stack empty? " + stack.empty());
+        // Check if the queue is empty
+        System.out.println("Is the queue empty? " + queue.isEmpty());
     }
 }
 
